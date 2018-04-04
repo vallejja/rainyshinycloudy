9377a23c400aa2b047c0d727f11f10ddaf436c1f
vallejja <julio.vallejo@mapleleaf.com>
2018-04-04 00:32:43 -0400

    Functional UI Commit

---
+++ rainyshinycloudy/Controller/WeatherVC.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  rainyshinycloudy
+//
+//  Created by Julio Vallejo on 2018-01-20.
+//  Copyright © 2018 Julio Vallejo. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class WeatherVC: UIViewController, UITableViewDataSource {
+
+    @IBOutlet weak var dateLbl: UILabel!
+    @IBOutlet weak var currentTempLbl: UILabel!
+    @IBOutlet weak var currentWeatherImg: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLbl: UILabel!
+    @IBOutlet weak var locationLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.cellForRow(at: indexPath)!
+    }
+    
+}
+
